805ef86a0aab697836fe7721ec164080414b225d
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-07-29 10:58:28 +0100

    Get latest blog posts

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 import jinja2
 import yaml
+import feedparser
+from datetime import datetime
 
 # Set up templates
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
@@ -7,10 +9,14 @@ templateEnv = jinja2.Environment(loader=templateLoader)
 template = templateEnv.get_template("TEMPLATE.md")
 
 # Load config
-with open('config.yaml') as f:
+with open("config.yaml") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+# Get latest blog post
+blog_rss = feedparser.parse("https://www.jacobtomlinson.co.uk/feed.xml")
+config["latest_blog_posts"] = blog_rss["entries"][0:5]
+for post in config["latest_blog_posts"]:
+    post["published"] = datetime.strptime(post["published"], "%a, %d %b %Y %H:%M:%S %z")
+
 # Render remplate
-print(template.render(
-    **config
-))
+print(template.render(**config))
